fea3e03be475c4870bf9de5067118df9da6a2a7e
Jeremy-Nourri <nourri.jer@gmail.com>
2024-06-19 16:16:00 +0200

    refactor: CarController

--- src/main/java/org/example/jaxrs/controller/CarController.java
+++ src/main/java/org/example/jaxrs/controller/CarController.java
@@ -8,20 +8,20 @@ import org.example.jaxrs.service.CarService;
 import java.util.List;
 
 @Path("/cars")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CarController {
 
     CarService carService = new CarService();
 
-
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Car> cars() {
         return carService.getCars();
     }
 
     @POST
     @Path("/create")
-    @Produces(MediaType.APPLICATION_JSON)
+
     public boolean createCar(@FormParam("id") int id,@FormParam("brand") String brand, @FormParam("year") int year, @FormParam("color") String color) {
         carService.createCar(id, brand, year, color);
            return true;
@@ -29,14 +29,12 @@ public class CarController {
 
     @PUT
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Car updateCar(@PathParam("id") int id, Car car) {
         return carService.updateCar(id, car);
     }
 
     @DELETE
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public String deleteCar(@PathParam("id") int id) {
         return carService.deleteCar(id);
     }
